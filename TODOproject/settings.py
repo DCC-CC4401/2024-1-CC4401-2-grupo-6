173f3b8ff34e234a82f3ad1b81e3275e9aea0d9d
cpereiram <cristobal.pereira@ug.uchile.cl>
2024-05-17 13:01:45 -0400

    Style update

--- TODOproject/settings.py
+++ TODOproject/settings.py
@@ -132,6 +132,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# En desarrollo, para servir archivos estáticos directamente
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # Simplified static file serving.
