8108626afdbc20c2b9389dbabbb0c2231b30b8ce
cpereiram <cristobal.pereira@ug.uchile.cl>
2024-07-16 00:48:24 -0400

    Se arregla menu en modo movil.

--- todoapp/static/js/home.js
+++ todoapp/static/js/home.js
@@ -1,29 +1,15 @@
 // script.js
 
-document.addEventListener('DOMContentLoaded', function() {
-  const mobileMenuBtn = document.getElementById('mobile-menu-btn');
-  const mobileMenu = document.createElement('div');
-  const overlay = document.createElement('div');
-  
-  mobileMenu.classList.add('mobile-menu');
-  mobileMenu.innerHTML = `
-      <div class="user-info">
-          <span class="username">Usuario</span>
-          <a href="#" class="logout-btn">Cerrar Sesión</a>
-      </div>
-  `;
-  document.body.appendChild(mobileMenu);
+const mobileMenuBtn = document.getElementById('mobile-menu-btn');
+const mobileMenu = document.getElementById('mobile-menu-content');
+const overlay = document.getElementById('overlay');
 
-  overlay.classList.add('overlay');
-  document.body.appendChild(overlay);
+mobileMenuBtn.addEventListener('click', function() {
+    mobileMenu.classList.toggle('show');
+    overlay.classList.toggle('show');
+});
 
-  mobileMenuBtn.addEventListener('click', function() {
-      mobileMenu.classList.toggle('show');
-      overlay.classList.toggle('show');
-  });
-
-  overlay.addEventListener('click', function() {
-      mobileMenu.classList.remove('show');
-      overlay.classList.remove('show');
-  });
+overlay.addEventListener('click', function() {
+    mobileMenu.classList.remove('show');
+    overlay.classList.remove('show');
 });
